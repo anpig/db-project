ee1ff24381e8f0be570540219da3832fabf51c2b
Bruce-0015 <brucelin.cs09@nycu.edu.tw>
2022-05-11 12:14:26 +0800

    Added:register.php check sign-up form

---
+++ register.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+$_SESSION['logged']=false;
+
+try {
+    if (empty($_POST['name']) || empty($_POST['phonenumber']) || empty($_POST['Account']) || empty($_POST['password']) || empty($_POST['re-password']) || empty($_POST['latitude']) || empty($_POST['longitude'])) {
+        $err_message="";
+        if(empty($_POST['name'])) $err_message=$err_message."NAME ";
+        if(empty($_POST['phonenumber'])) $err_message=$err_message."PHONENUMBER ";
+        if(empty($_POST['Account'])) $err_message=$err_message."ACCOUNT ";
+        if(empty($_POST['password'])) $err_message=$err_message."PASSWORD ";
+        if(empty($_POST['re-password'])) $err_message=$err_message."RETYPE-PASSWORD ";
+        if(empty($_POST['latitude'])) $err_message=$err_message."LATITUDE ";
+        if(empty($_POST['longitude'])) $err_message=$err_message."LONGTITUDE "; 
+        throw new Exception("空白欄位:$err_message");
+    }
+    if ($_POST['password'] != $_POST['re-password']) {
+        throw new Exception('密碼驗證 ≠ 密碼');
+    }
+    if (!ctype_alnum($_POST['Account']) || !ctype_alnum($_POST['password']) || !ctype_alpha(str_replace(' ', '', $_POST['name'])) || strlen($_POST['phonenumber']) != 10 || !ctype_digit($_POST['phonenumber']) || $_POST['latitude'] > 90 || $_POST['latitude'] < -90 || $_POST['longitude'] > 180 || $_POST['longitude'] < -180) {
+        $err_message="";
+        if(!ctype_alnum($_POST['Account'])) $err_message=$err_message."帳號組成僅包含大小寫英文與數字 ";
+        if(!ctype_alnum($_POST['password'])) $err_message=$err_message."密碼組成僅包含大小寫英文與數字 ";
+        if(!ctype_alpha(str_replace(' ', '', $_POST['name']))) $err_message=$err_message."名字組成僅由大小寫英文 ";
+        if(strlen($_POST['phonenumber']) != 10 || !ctype_digit($_POST['phonenumber'])) $err_message=$err_message."手機號碼需為 10 位數字 ";
+        if($_POST['latitude'] > 90 || $_POST['latitude'] < -90 || $_POST['longitude'] > 180 || $_POST['longitude'] < -180) $err_message=$err_message."經緯度範圍不正確 ";
+        throw new Exception("輸入格式不對:$err_message");
+    }
+} catch (Exception $e) {
+    $msg=$e->getMessage();
+    session_unset();
+    session_destroy();
+
+    echo <<<EOT
+    <!DOCTYPE html>
+        <html>
+            <body>
+                <script>
+                    alert("$msg");
+                    window.location.replace("sign-up.php");
+                </script>
+            </body>
+        </html>
+    EOT;
+}

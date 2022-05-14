8127ff803e69e569a53e891132dac11007424099
Bruce-0015 <brucelin.cs09@nycu.edu.tw>
2022-05-14 22:31:20 +0800

    add connection function

--- connect.php
+++ connect.php
@@ -2,10 +2,28 @@
 try{
     $conn = new PDO("mysql:host=localhost;dbname=db-project","root","");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->query('select * from user');
+    foreach($stmt as $row){
+        echo $row['username']." ".$row['password']."<br>";
+    }
     echo "connected";
     $conn = null;
-} catch (PDOException $e) {
-    die ("Error!: " . $e->getMessage() . "<br/>");
+} catch (Exception $e) {
+    $msg=$e->getMessage();
+    session_unset();
+    session_destroy();
+
+    echo <<<EOT
+    <!DOCTYPE html>
+        <html>
+            <body>
+                <script>
+                    alert("$msg");
+                    window.location.replace("connect.php");
+                </script>
+            </body>
+        </html>
+    EOT;
 }
 
 ?>

945290599921ff3ab825ffbffff2f8109e5ad408
Bruce-0015 <brucelin.cs09@nycu.edu.tw>
2022-05-14 23:06:30 +0800

    add new line in error message

--- register.php
+++ register.php
@@ -11,27 +11,27 @@
     try {
         if (empty($_POST['name']) || empty($_POST['phonenumber']) || empty($_POST['account']) || empty($_POST['password']) || empty($_POST['re-password']) || empty($_POST['latitude']) || empty($_POST['longitude'])) {
             $err_message="";
-            if (empty($_POST['name'])) $err_message=$err_message."NAME ";
-            if (empty($_POST['phonenumber'])) $err_message=$err_message."PHONENUMBER ";
-            if (empty($_POST['account'])) $err_message=$err_message."ACCOUNT ";
-            if (empty($_POST['password'])) $err_message=$err_message."PASSWORD ";
-            if (empty($_POST['re-password'])) $err_message=$err_message."RETYPE-PASSWORD ";
-            if (empty($_POST['latitude'])) $err_message=$err_message."LATITUDE ";
-            if (empty($_POST['longitude'])) $err_message=$err_message."LONGTITUDE "; 
-            throw new Exception("空白欄位:$err_message");
+            if (empty($_POST['name'])) $err_message=$err_message."NAME".'\n';
+            if (empty($_POST['phonenumber'])) $err_message=$err_message."PHONENUMBER".'\n';
+            if (empty($_POST['account'])) $err_message=$err_message."ACCOUNT".'\n';
+            if (empty($_POST['password'])) $err_message=$err_message."PASSWORD".'\n';
+            if (empty($_POST['re-password'])) $err_message=$err_message."RETYPE-PASSWORD".'\n';
+            if (empty($_POST['latitude'])) $err_message=$err_message."LATITUDE".'\n';
+            if (empty($_POST['longitude'])) $err_message=$err_message."LONGTITUDE".'\n'; 
+            throw new Exception('空白欄位:'.'\n'."$err_message");
         }
         else if ($_POST['password'] != $_POST['re-password']) {
             throw new Exception('密碼驗證 ≠ 密碼');
         }
         else if (!ctype_alnum($_POST['account']) || !ctype_alnum($_POST['password']) || !ctype_alpha(str_replace(' ', '', $_POST['name'])) || strlen($_POST['phonenumber']) != 10 || !ctype_digit($_POST['phonenumber']) || $_POST['latitude'] > 90 || $_POST['latitude'] < -90 || $_POST['longitude'] > 180 || $_POST['longitude'] < -180) {
             $err_message="";
-            if (!ctype_alnum($_POST['account'])) $err_message=$err_message."帳號組成僅包含大小寫英文與數字 ";
-            if (!ctype_alnum($_POST['password'])) $err_message=$err_message."密碼組成僅包含大小寫英文與數字 ";
-            if (!ctype_alpha(str_replace(' ', '', $_POST['name']))) $err_message=$err_message."名字組成僅由大小寫英文 ";
-            if (strlen($_POST['phonenumber']) != 10 || !ctype_digit($_POST['phonenumber'])) $err_message=$err_message."手機號碼需為 10 位數字 ";
-            if ($_POST['latitude'] > 90 || $_POST['latitude'] < -90 || $_POST['longitude'] > 180 || $_POST['longitude'] < -180) $err_message=$err_message."經緯度範圍不正確 ";
+            if (!ctype_alnum($_POST['account'])) $err_message=$err_message."帳號組成僅包含大小寫英文與數字".'\n';
+            if (!ctype_alnum($_POST['password'])) $err_message=$err_message."密碼組成僅包含大小寫英文與數字".'\n';
+            if (!ctype_alpha(str_replace(' ', '', $_POST['name']))) $err_message=$err_message."名字組成僅由大小寫英文".'\n';
+            if (strlen($_POST['phonenumber']) != 10 || !ctype_digit($_POST['phonenumber'])) $err_message=$err_message."手機號碼需為 10 位數字".'\n';
+            if ($_POST['latitude'] > 90 || $_POST['latitude'] < -90 || $_POST['longitude'] > 180 || $_POST['longitude'] < -180) $err_message=$err_message."經緯度範圍不正確".'\n';
             if (!ctype_digit($_POST['latitude']) || !ctype_digit($_POST['longitude'])) $err_message=$err_message."經緯度應是數字 ";
-            throw new Exception("輸入格式不對：$err_message");
+            throw new Exception("輸入格式不對：".'\n'."$err_message");
         }
         else {
             $name = $_POST['name'];

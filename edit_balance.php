781da6050afe90320d38fa51f7ff8d3357279aa9
Bruce-0015 <brucelin.cs09@nycu.edu.tw>
2022-05-15 02:49:37 +0800

    Added:edit_balance.php add balance

---
+++ edit_balance.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <body>
+<?php
+    session_start();
+    $dbservername='localhost';
+    $dbname='db-project';
+    $dbusername='db';
+    $dbpassword='db';
+    try {
+        if (empty($_POST['value'])) {
+            throw new Exception('空白欄位');
+        }
+        else if (!is_numeric($_POST['value'])) {
+            throw new Exception("輸入格式不對");
+        }
+        else {
+            $value = $_POST['value'];
+            $UID=$_SESSION['UID'];
+            $db = new PDO("mysql:host=$dbservername;dbname=$dbname", $dbusername, $dbpassword); // connect to db
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);   // set the PDO error mode to exception
+            $sql = $db->prepare("select * from user where UID=:UID");
+            $sql->execute(array('UID' => $UID));
+            $row = $sql->fetch();
+            
+            $odd=$row['balance'];
+            $new=$value+$odd;
+
+            $sql = $db->prepare("UPDATE user SET balance=:new WHERE `user`.`UID` = :UID");
+            $sql->execute(array('new' => $new, 'UID' => $UID));
+            throw new Exception("ADDED Success!");
+        }
+    } catch (Exception $e) {
+        $msg=$e->getMessage();
+        echo "<script>alert(\"$msg\"); window.location.replace(\"nav.php\");</script>";
+    }
+?>
+    </body>
+</html>

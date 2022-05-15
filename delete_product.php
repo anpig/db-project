8be3d9f86c1edcb0f5f408a763b77f0ae060676d
Bruce-0015 <brucelin.cs09@nycu.edu.tw>
2022-05-16 00:28:35 +0800

    Added: delete_product.php add function of deleting product

---
+++ delete_product.php
@@ -0,0 +1,27 @@
+<?php
+  session_start();
+  if (!isset($_SESSION['logged']) || $_SESSION['logged'] == false) {
+    header('Location: .');
+    die();
+  }
+  $PID = $_SESSION['PID'];
+  $_SESSION['PID'] = null;
+  $UID = $_SESSION['UID'];
+  $dbservername='localhost';
+  $dbname='db-project';
+  $dbusername='db';
+  $dbpassword='db';
+
+  try { 
+    $db = new PDO("mysql:host=$dbservername;dbname=$dbname", $dbusername, $dbpassword); // connect to db
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);   // set the PDO error mode to exception
+
+    $sql = $db->prepare("DELETE FROM `product` WHERE `product`.`PID` = :PID");
+    $sql->execute(array('PID' => $PID));
+    throw new Exception("DELETE Success!");
+    header('Location: nav.php#shop');
+  } catch (Exception $e) {
+      $msg=$e->getMessage();
+      echo "<script>alert(\"$msg\"); window.location.replace(\"nav.php#shop\");</script>";
+  }
+?>

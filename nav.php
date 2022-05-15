6ee4dc49c319775a058e8d08fd6f229440473d29
anpig <31090467+anpig@users.noreply.github.com>
2022-05-15 16:46:38 +0800

    Fixed nav.php fixed shitty indents and capitalize some word

--- nav.php
+++ nav.php
@@ -39,7 +39,6 @@
 </head>
 
 <body>
- 
   <nav class="navbar navbar-inverse">
     <div class="container-fluid">
       <div class="navbar-header">
@@ -64,8 +63,8 @@
               echo "帳號: $account 名字: $name 手機: $phone_number 經度: $location_longitude 緯度: $location_latitude";
             ?>
 
-            <button type="button " style="margin-left: 5px;" class=" btn btn-info " data-toggle="modal"
-            data-target="#location">edit location</button>
+            <button type="button " style="margin-left: 5px; margin-right: 10px;" class=" btn btn-info " data-toggle="modal"
+            data-target="#location">Edit Location</button>
             <!--  -->
             <div class="modal fade" id="location"  data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
               <div class="modal-dialog  modal-sm">
@@ -73,13 +72,13 @@
                   <div class="modal-content">
                     <div class="modal-header">
                       <button type="button" class="close" data-dismiss="modal">&times;</button>
-                      <h4 class="modal-title">edit location</h4>
+                      <h4 class="modal-title">Edit Location</h4>
                     </div>
                     <div class="modal-body form-group">
-                      <label class="control-label" for="latitude">latitude</label>
+                      <label class="control-label" for="latitude">Latitude</label>
                       <input type="text" name="latitude" class="form-control" id="latitude" placeholder="enter latitude">
                       <br>
-                      <label class="control-label" for="longitude">longitude</label>
+                      <label class="control-label" for="longitude">Longitude</label>
                       <input type="text" name="longitude" class="form-control" id="longitude" placeholder="enter longitude">
                     </div>
                     <div class="modal-footer form-group">
@@ -90,21 +89,18 @@
                 </form>
               </div>
             </div>
-
-
-
             <!--  -->
-            walletbalance: <?php echo $row['balance']; ?>
+            <span>Wallet Balance: <?php echo $row['balance']; ?></span>
             <!-- Modal -->
             <button type="button " style="margin-left: 5px;" class=" btn btn-info " data-toggle="modal"
-              data-target="#myModal">Add value</button>
+              data-target="#myModal">Add Value</button>
             <div class="modal fade" id="myModal"  data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
               <div class="modal-dialog  modal-sm">
                 <form action="edit_balance.php" method="post">
                   <div class="modal-content">
                     <div class="modal-header">
                       <button type="button" class="close" data-dismiss="modal">&times;</button>
-                      <h4 class="modal-title">Add value</h4>
+                      <h4 class="modal-title">Add Value</h4>
                     </div>
                     <div class="modal-body">
                       <input type="text" class="form-control" id="value" name="value" placeholder="enter add value">
@@ -117,12 +113,7 @@
               </div>
             </div>
           </div>
-
         </div>
-
-        <!-- 
-                
-             -->
         <h3>Search</h3>
         <div class=" row  col-xs-8">
           <form class="form-horizontal" action="/action_page.php">
@@ -131,33 +122,23 @@
               <div class="col-sm-5">
                 <input type="text" class="form-control" placeholder="Enter Shop name">
               </div>
-              <label class="control-label col-sm-1" for="distance">distance</label>
+              <label class="control-label col-sm-1" for="distance">Distance</label>
               <div class="col-sm-5">
-
-
                 <select class="form-control" id="sel1">
-                  <option>near</option>
-                  <option>medium </option>
-                  <option>far</option>
-
+                  <option>Near</option>
+                  <option>Medium </option>
+                  <option>Far</option>
                 </select>
               </div>
-
             </div>
-
             <div class="form-group">
-
               <label class="control-label col-sm-1" for="Price">Price</label>
               <div class="col-sm-2">
-
                 <input type="text" class="form-control">
-
               </div>
               <label class="control-label col-sm-1" for="~">~</label>
               <div class="col-sm-2">
-
                 <input type="text" class="form-control">
-
               </div>
               <label class="control-label col-sm-1" for="Meal">Meal</label>
               <div class="col-sm-5">
@@ -168,20 +149,15 @@
                 </datalist>
               </div>
             </div>
-
             <div class="form-group">
-              <label class="control-label col-sm-1" for="category"> category</label>
-            
-              
+              <label class="control-label col-sm-1" for="category"> Category</label>
                 <div class="col-sm-5">
                   <input type="text" list="categorys" class="form-control" id="category" placeholder="Enter shop category">
                   <datalist id="categorys">
                     <option value="fast food">
-               
                   </datalist>
                 </div>
                 <button type="submit" style="margin-left: 18px;"class="btn btn-primary">Search</button>
-              
             </div>
           </form>
         </div>
@@ -191,88 +167,74 @@
               <thead>
                 <tr>
                   <th scope="col">#</th>
-                
-                  <th scope="col">shop name</th>
-                  <th scope="col">shop category</th>
+                  <th scope="col">Shop Name</th>
+                  <th scope="col">Shop Category</th>
                   <th scope="col">Distance</th>
-               
                 </tr>
               </thead>
               <tbody>
                 <tr>
                   <th scope="row">1</th>
-               
                   <td>macdonald</td>
                   <td>fast food</td>
-                
                   <td>near </td>
-                  <td>  <button type="button" class="btn btn-info " data-toggle="modal" data-target="#macdonald">Open menu</button></td>
-            
+                  <td><button type="button" class="btn btn-info " data-toggle="modal" data-target="#macdonald">Open menu</button></td>
                 </tr>
-           
-
               </tbody>
             </table>
-
-                <!-- Modal -->
-  <div class="modal fade" id="macdonald"  data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
-    
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">menu</h4>
-        </div>
-        <div class="modal-body">
-         <!--  -->
-  
-         <div class="row">
-          <div class="  col-xs-12">
-            <table class="table" style=" margin-top: 15px;">
-              <thead>
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Picture</th>
-                 
-                  <th scope="col">meal name</th>
-               
-                  <th scope="col">price</th>
-                  <th scope="col">Quantity</th>
-                
-                  <th scope="col">Order check</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th scope="row">1</th>
-                  <td><img src="Picture/1.jpg" with="50" heigh="10" alt="Hamburger"></td>
-                
-                  <td>Hamburger</td>
-                
-                  <td>80 </td>
-                  <td>20 </td>
-              
-                  <td> <input type="checkbox" id="cbox1" value="Hamburger"></td>
-                </tr>
-                <tr>
-                  <th scope="row">2</th>
-                  <td><img src="Picture/2.jpg" with="10" heigh="10" alt="coffee"></td>
-                 
-                  <td>coffee</td>
-             
-                  <td>50 </td>
-                  <td>20</td>
-              
-                  <td><input type="checkbox" id="cbox2" value="coffee"></td>
-                </tr>
-
-              </tbody>
-            </table>
-          </div>
-
-        </div>
-         <!--  -->
+            <!-- Modal -->
+            <div class="modal fade" id="macdonald"  data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+              <div class="modal-dialog">
+                <!-- Modal content-->
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">menu</h4>
+                  </div>
+                  <div class="modal-body">
+                    <div class="row">
+                      <div class="  col-xs-12">
+                        <table class="table" style=" margin-top: 15px;">
+                          <thead>
+                            <tr>
+                              <th scope="col">#</th>
+                              <th scope="col">Picture</th>
+                            
+                              <th scope="col">meal name</th>
+                          
+                              <th scope="col">price</th>
+                              <th scope="col">Quantity</th>
+                            
+                              <th scope="col">Order check</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <th scope="row">1</th>
+                              <td><img src="Picture/1.jpg" with="50" heigh="10" alt="Hamburger"></td>
+                            
+                              <td>Hamburger</td>
+                            
+                              <td>80 </td>
+                              <td>20 </td>
+                          
+                              <td> <input type="checkbox" id="cbox1" value="Hamburger"></td>
+                            </tr>
+                            <tr>
+                              <th scope="row">2</th>
+                              <td><img src="Picture/2.jpg" with="10" heigh="10" alt="coffee"></td>
+                            
+                              <td>coffee</td>
+                        
+                              <td>50 </td>
+                              <td>20</td>
+                          
+                              <td><input type="checkbox" id="cbox2" value="coffee"></td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
                   </div>
                   <div class="modal-footer">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Order</button>

d22e72d0b116a067615756c0ab863ab86a480587
anpig <pkid0anpig@gmail.com>
2022-05-15 12:39:54 +0800

    Edited nav.php separated "shop" pane depends on role

--- nav.php
+++ nav.php
@@ -288,142 +288,128 @@
         </div>
       </div>
       <div id="menu1" class="tab-pane fade">
-
-        <h3> Start a business </h3>
-        <div class="form-group ">
-          <div class="row">
-            <div class="col-xs-2">
-              <label for="ex5">shop name</label>
-              <input class="form-control" id="ex5" placeholder="macdonald" type="text" >
-            </div>
-            <div class="col-xs-2">
-              <label for="ex5">shop category</label>
-              <input class="form-control" id="ex5" placeholder="fast food" type="text" >
-            </div>
-            <div class="col-xs-2">
-              <label for="ex6">latitude</label>
-              <input class="form-control" id="ex6" placeholder="121.00028167648875" type="text" >
-            </div>
-            <div class="col-xs-2">
-              <label for="ex8">longitude</label>
-              <input class="form-control" id="ex8" placeholder="24.78472733371133" type="text" >
-            </div>
-          </div>
-        </div>
-
-
-
-        <div class=" row" style=" margin-top: 25px;">
-          <div class=" col-xs-3">
-            <button type="button" class="btn btn-primary"  >register</button>
-          </div>
-        </div>
-        <hr>
-        <h3>ADD</h3>
-
-        <div class="form-group ">
-          <div class="row">
-
-            <div class="col-xs-6">
-              <label for="ex3">meal name</label>
-              <input class="form-control" id="ex3" type="text">
-            </div>
-          </div>
-          <div class="row" style=" margin-top: 15px;">
-            <div class="col-xs-3">
-              <label for="ex7">price</label>
-              <input class="form-control" id="ex7" type="text">
-            </div>
-            <div class="col-xs-3">
-              <label for="ex4">quantity</label>
-              <input class="form-control" id="ex4" type="text">
-            </div>
-          </div>
-
-
-          <div class="row" style=" margin-top: 25px;">
-
-            <div class=" col-xs-3">
-              <label for="ex12">上傳圖片</label>
-              <input id="myFile" type="file" name="myFile" multiple class="file-loading">
-
-            </div>
-            <div class=" col-xs-3">
-
-              <button style=" margin-top: 15px;" type="button" class="btn btn-primary">Add</button>
-            </div>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="  col-xs-8">
-            <table class="table" style=" margin-top: 15px;">
-              <thead>
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Picture</th>
-                  <th scope="col">meal name</th>
-              
-                  <th scope="col">price</th>
-                  <th scope="col">Quantity</th>
-                  <th scope="col">Edit</th>
-                  <th scope="col">Delete</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th scope="row">1</th>
-                  <td><img src="Picture/1.jpg" with="50" heigh="10" alt="Hamburger"></td>
-                  <td>Hamburger</td>
-                
-                  <td>80 </td>
-                  <td>20 </td>
-                  <td><button type="button" class="btn btn-info" data-toggle="modal" data-target="#Hamburger-1">
-                  Edit
-                  </button></td>
-                  <!-- Modal -->
-                      <div class="modal fade" id="Hamburger-1" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                          <div class="modal-content">
-                            <div class="modal-header">
-                              <h5 class="modal-title" id="staticBackdropLabel">Hamburger Edit</h5>
-                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                              </button>
-                            </div>
-                            <div class="modal-body">
-                              <div class="row" >
-                                <div class="col-xs-6">
-                                  <label for="ex71">price</label>
-                                  <input class="form-control" id="ex71" type="text">
-                                </div>
-                                <div class="col-xs-6">
-                                  <label for="ex41">quantity</label>
-                                  <input class="form-control" id="ex41" type="text">
+        <?php
+          if (true) {
+            echo <<< EOT
+              <h3> Start a business </h3>
+              <div class="form-group ">
+                <div class="row">
+                  <div class="col-xs-2">
+                    <label for="ex5">shop name</label>
+                    <input class="form-control" id="ex5" placeholder="macdonald" type="text" >
+                  </div>
+                  <div class="col-xs-2">
+                    <label for="ex5">shop category</label>
+                    <input class="form-control" id="ex5" placeholder="fast food" type="text" >
+                  </div>
+                  <div class="col-xs-2">
+                    <label for="ex6">latitude</label>
+                    <input class="form-control" id="ex6" placeholder="121.00028167648875" type="text" >
+                  </div>
+                  <div class="col-xs-2">
+                    <label for="ex8">longitude</label>
+                    <input class="form-control" id="ex8" placeholder="24.78472733371133" type="text" >
+                  </div>
+                </div>
+              </div>
+              <div class=" row" style=" margin-top: 25px;">
+                <div class=" col-xs-3">
+                  <button type="button" class="btn btn-primary"  >register</button>
+                </div>
+              </div>
+              <hr>
+            EOT;
+          }
+          else {
+            echo <<< EOT
+              <h3>ADD</h3>
+              <div class="form-group ">
+                <div class="row">
+                  <div class="col-xs-6">
+                    <label for="ex3">meal name</label>
+                    <input class="form-control" id="ex3" type="text">
+                  </div>
+                </div>
+                <div class="row" style=" margin-top: 15px;">
+                  <div class="col-xs-3">
+                    <label for="ex7">price</label>
+                    <input class="form-control" id="ex7" type="text">
+                  </div>
+                  <div class="col-xs-3">
+                    <label for="ex4">quantity</label>
+                    <input class="form-control" id="ex4" type="text">
+                  </div>
+                </div>
+                <div class="row" style=" margin-top: 25px;">
+                  <div class=" col-xs-3">
+                    <label for="ex12">上傳圖片</label>
+                    <input id="myFile" type="file" name="myFile" multiple class="file-loading">
+                  </div>
+                  <div class=" col-xs-3">
+                    <button style=" margin-top: 15px;" type="button" class="btn btn-primary">Add</button>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="  col-xs-8">
+                  <table class="table" style="margin-top: 15px;">
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Picture</th>
+                        <th scope="col">meal name</th>
+                        <th scope="col">price</th>
+                        <th scope="col">Quantity</th>
+                        <th scope="col">Edit</th>
+                        <th scope="col">Delete</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <th scope="row">1</th>
+                        <td><img src="Picture/1.jpg" with="50" heigh="10" alt="Hamburger"></td>
+                        <td>Hamburger</td>
+                        <td>80 </td>
+                        <td>20 </td>
+                        <td><button type="button" class="btn btn-info" data-toggle="modal" data-target="#Hamburger-1">Edit</button></td>
+                        <!-- Modal -->
+                        <div class="modal fade" id="Hamburger-1" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                          <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                              <div class="modal-header">
+                                <h5 class="modal-title" id="staticBackdropLabel">Hamburger Edit</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                  <span aria-hidden="true">&times;</span>
+                                </button>
+                              </div>
+                              <div class="modal-body">
+                                <div class="row" >
+                                  <div class="col-xs-6">
+                                    <label for="ex71">price</label>
+                                    <input class="form-control" id="ex71" type="text">
+                                  </div>
+                                  <div class="col-xs-6">
+                                    <label for="ex41">quantity</label>
+                                    <input class="form-control" id="ex41" type="text">
+                                  </div>
                                 </div>
                               </div>
-                    
-                            </div>
-                            <div class="modal-footer">
-                              <button type="button" class="btn btn-secondary" data-dismiss="modal">Edit</button>
-                             
+                              <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Edit</button>
+                              </div>
                             </div>
                           </div>
                         </div>
-                      </div>
-                  <td><button type="button" class="btn btn-danger">Delete</button></td>
-                </tr>
-                <tr>
-                  <th scope="row">2</th>
-                  <td><img src="Picture/2.jpg" with="10" heigh="10" alt="coffee"></td>
-                  <td>coffee</td>
-               
-                  <td>50 </td>
-                  <td>20</td>
-                  <td><button type="button" class="btn btn-info" data-toggle="modal" data-target="#coffee-1">
-                    Edit
-                    </button></td>
-                    <!-- Modal -->
+                        <td><button type="button" class="btn btn-danger">Delete</button></td>
+                      </tr>
+                      <tr>
+                        <th scope="row">2</th>
+                        <td><img src="Picture/2.jpg" with="10" heigh="10" alt="coffee"></td>
+                        <td>coffee</td>
+                        <td>50 </td>
+                        <td>20</td>
+                        <td><button type="button" class="btn btn-info" data-toggle="modal" data-target="#coffee-1">Edit</button></td>
+                          <!-- Modal -->
                         <div class="modal fade" id="coffee-1" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                           <div class="modal-dialog" role="document">
                             <div class="modal-content">
@@ -448,30 +434,22 @@
                               </div>
                               <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Edit</button>
-                               
                               </div>
                             </div>
                           </div>
                         </div>
-
-
-                  <td><button type="button" class="btn btn-danger">Delete</button></td>
-                </tr>
-
-              </tbody>
-            </table>
-          </div>
-
-        </div>
-
-
+                        <td><button type="button" class="btn btn-danger">Delete</button></td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            EOT;
+          }
+        ?>
       </div>
-
-
-
     </div>
   </div>
-
   <!-- Option 1: Bootstrap Bundle with Popper -->
   <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> -->
   <script>

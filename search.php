20253318deb3743b484ef0c0e2720946f134db46
anpig <31090467+anpig@users.noreply.github.com>
2022-05-16 20:32:25 +0800

    Edited search.php now filter can filter category and meal

--- search.php
+++ search.php
@@ -14,15 +14,17 @@
         $sql->execute(array('UID' => $UID));
         $row = $sql->fetch();
         $user_latitude = $row['location_latitude']; $user_longitude = $row['location_longitude'];
-
         $somethingisset = false;
-        $querystring = "SELECT SID, shopname, category, location_longitude, location_latitude FROM shop WHERE shopname LIKE :shopname";
-        if (isset($_REQUEST['shopname'])) {
-            if ($somethingisset) $querystring .= " AND ";
-            $shopname = "%" . $_REQUEST['shopname'] . "%";
-            $somethingisset = true;
-        }
+        $querystring = "SELECT SID, shopname, category, location_longitude, location_latitude FROM shop 
+                        WHERE shopname LIKE :shopname AND category LIKE :category AND SID=(
+                            SELECT SID FROM product WHERE product_name LIKE :meal)
+                        )";
+        if (isset($_REQUEST['shopname'])) $shopname = "%" . $_REQUEST['shopname'] . "%";
         else $shopname = "%%";
+        if (isset($_REQUEST['category'])) $category = "%" . $_REQUEST['category'] . "%";
+        else $category = "%%";
+        if (isset($_REQUEST['meal'])) $meal = "%" . $_REQUEST['meal'] . "%";
+        else $meal = "%%";
         // if (isset($_REQUEST['distance'])) {
         //     if ($somethingisset) $querystring += " AND ";
         //     if ($_REQUEST['distance'] == "near") $querystring += ""
@@ -31,7 +33,9 @@
         // }
         $sql = $db->prepare($querystring);
         $sql->execute(array(
-            'shopname' => $shopname
+            'shopname' => $shopname,
+            'category' => $category,
+            'meal' => $meal
         ));
         $result = $sql->fetchAll();
         foreach ($result as &$row) {

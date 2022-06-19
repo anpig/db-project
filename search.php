e18c6b081965989a979acec39a761e7f3845fdb9
Bruce-0015 <brucelin.cs09@nycu.edu.tw>
2022-06-20 01:16:33 +0800

    feat: add function of calculating price

--- search.php
+++ search.php
@@ -13,6 +13,7 @@
         $sql->execute(array('UID' => $UID));
         $row = $sql->fetch();
         $user_latitude = $row['location_latitude']; $user_longitude = $row['location_longitude'];
+        $dist = [];
         
         if (isset($_REQUEST['shopname'])) $shopname = "%" . $_REQUEST['shopname'] . "%";
         else $shopname = "%%";
@@ -67,7 +68,7 @@
                         <th width="10%" scope="col"></th>
                     </tr>
                 </thead>
-                <tbody>
+            <tbody>
         EOT;
         $SID_arr = array();
         foreach ($result as &$row) {
@@ -77,6 +78,7 @@
             $shop_latitude = $row['location_latitude'];
             $shop_longitude = $row['location_longitude'];
             $dis = cal_distance($user_latitude, $user_longitude, $shop_latitude, $shop_longitude);
+            $dist["$SID"] = $dis;
             $distanceWord = "";
             if (isset($_REQUEST['distance'])) {
                 if ($_REQUEST['distance'] == "near") {
@@ -102,15 +104,16 @@
                     <td>$shopname</td>
                     <td>$category</td>
                     <td>$distanceWord</td>
-                    <td><button type="button" class="btn btn-info " data-toggle="modal" data-target="#$SID">Open menu</button></td>
+                    <td><button type="button" class="btn btn-info" data-toggle="modal" data-target="#$SID">Open menu</button></td>
                 </tr>
+            </tbody>
             EOT;
             array_push($SID_arr, $SID);
         }
         foreach ($SID_arr as $SID) {
+            $this_dist = $dist["$SID"];
             echo <<< EOT
-                </tbody>
-                    </table>
+                </table>
                     <!-- Modal -->
                         <div class="modal fade" id=$SID  data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                             <div class="modal-dialog">
@@ -120,6 +123,7 @@
                                         <button type="button" class="close" data-dismiss="modal">&times;</button>
                                         <h4 class="modal-title">Menu</h4>
                                     </div>
+                                <form action="cal_price.php" class="fh5co-form animate-box" data-animate-effect="fadeIn" method="post">
                                     <div class="modal-body">
                                         <div class="row">
                                             <div class="  col-xs-12">
@@ -130,37 +134,46 @@
                                                             <th scope="col">Meal Name</th>
                                                             <th scope="col">Price</th>
                                                             <th scope="col">Quantity</th>
-                                                            <th scope="col">Order check</th>
+                                                            <th scope="col">Order</th>
                                                         </tr>
                                                     </thead>
-                                                <tbody>
+                                                    <tbody>
             EOT;
-            $sql = $db->query("SELECT * FROM product WHERE SID=$SID");
-            $shoprow = $sql->fetchAll();
-            foreach ($shoprow as &$row) {
-                $product_name = $row['product_name'];
-                $price = $row['price'];
-                $quantity = $row['quantity'];
-                $picture = $row['picture'];
-                $picture_type = $row['picture_type'];
-                echo '<tr><td><img style="max-width:100%; max-height:200px" src="data:'.$picture_type.';base64,' . $picture . '" alt="$product_name"/></td>';
-                echo <<< EOT
-                        <td>$product_name</td>
-                        <td>$price</td>
-                        <td>$quantity</td>
-                        <td><input type="checkbox" id="#$SID" value="$product_name"></td>
-                    </tr>
-                EOT;
-            }
-            echo <<< EOT
-            </tbody>
-                </table>
-                    </div>
-                        </div>
-                            </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-default" data-dismiss="modal">Order</button>
-                                </div>
+                                                    $sql = $db->query("SELECT * FROM product WHERE SID=$SID");
+                                                    $shoprow = $sql->fetchAll();
+                                                    foreach ($shoprow as &$row) {
+                                                        $PID = $row['PID'];
+                                                        $product_name = $row['product_name'];
+                                                        $price = $row['price'];
+                                                        $quantity = $row['quantity'];
+                                                        $picture = $row['picture'];
+                                                        $picture_type = $row['picture_type'];
+                                                        echo '<tr><td><img style="max-width:100%; max-height:200px" src="data:'.$picture_type.';base64,' . $picture . '" alt="$product_name"/></td>';
+                                                        echo <<< EOT
+                                                                <td>$product_name</td>
+                                                                <td>$price</td>
+                                                                <td>$quantity</td>
+                                                                <td><input type="number" name="$PID" id="$PID" value=0 min=0 max="$quantity"></td>
+                                                            </tr>
+                                                        EOT;
+                                                    }
+                                                    echo <<< EOT
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <label class="control-label col-sm-1" for="type">Type</label>
+                                    <select name="select">
+                                        <option>Delivery</option>
+                                        <option>Pick-up</option>
+                                    </select>
+                                    <div class="form-group">
+                                        <input type="hidden" name="SID" value="$SID">
+                                        <input type="hidden" name="dist" value="$this_dist">
+                                        <input type="submit" value="Calculate the price" class="btn btn-primary">
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>

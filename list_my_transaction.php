9431d3a0a7715ea2a2a04350513f51b3ffb0c1a5
Bruce-0015 <brucelin.cs09@nycu.edu.tw>
2022-06-20 23:27:11 +0800

    feat: add  list_my_transaction.php

---
+++ list_my_transaction.php
@@ -0,0 +1,55 @@
+<?php
+    session_start();
+    $dbservername='localhost';
+    $dbname='db-project';
+    $dbusername='db';
+    $dbpassword='db';
+    $UID=$_SESSION['UID'];
+    include('cal_distance.php');
+    try {
+        $db = new PDO("mysql:host=$dbservername;dbname=$dbname", $dbusername, $dbpassword);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo <<< EOT
+            <table class="table" style="margin-top: 15px">
+                <thead>
+                    <tr>
+                        <th width="20%" scope="col">Record ID</th>
+                        <th width="20%" scope="col">Action</th>
+                        <th width="20%" scope="col">Time</th>
+                        <th width="20%" scope="col">Trader</th>
+                        <th width="20%" scope="col">Amount Change</th>
+                    </tr>
+                </thead>
+            <tbody>
+        EOT;
+        if (isset($_REQUEST['action'])) {
+            $action = $_REQUEST['action'];
+            $sql = $db->query("select * from transaction where UID = '$UID' and type = '$action'");
+        }
+        else {
+            $sql = $db->query("select * from transaction where UID = '$UID'");
+        }
+        $result = $sql->fetchAll();
+        foreach ($result as &$row) {
+            $record_id = $row['TID'];
+            $action = $row['type'];
+            $time = $row['time'];
+            $trader = $row['trader'];
+            $amount_change = $row['value'];
+            echo <<< EOT
+                <tr>
+                    <td>$record_id</td>
+                    <td>$action</td>
+                    <td>$time</td>
+                    <td>$trader</td>
+                    <td>$amount_change</td>
+                    </tr>
+                </tbody>
+            EOT;
+        }
+    }
+    catch (Exception $e) { 
+        $msg = $e->getMessage();
+        echo $msg;
+    }
+?>

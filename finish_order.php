ab5e638d48c064634f4bb3b8583e4d07e7e678c2
anpig <31090467+anpig@users.noreply.github.com>
2022-06-21 16:40:19 +0800

    fixed bug: #5

--- finish_order.php
+++ finish_order.php
@@ -11,9 +11,29 @@
 
     $result = $sql->fetch();
     $status = $result['status'];
-    if($status == 'canceled') {
+    if ($status == 'canceled') {
         echo "<script>alert(\"該訂單已被取消\"); window.location.replace(\"nav.php#shop_order\");</script>";
         exit();
+    }
+    else if {
+        $sql = $db->query("SELECT * FROM items WHERE OID=$OID");
+        $result = $sql->fetchAll();
+        foreach ($result as &$row) {
+            $PID = $row['PID'];
+            $order_quantity = $row['quantity'];
+            $sql = $db->query("SELECT * FROM product WHERE PID=$PID");
+            $tmp = $sql->fetchAll();
+            $product_existence = $tmp['listed'];
+            $product_quantity = $tmp['quantity'];
+            if ($order_quantity > $product_quantity) {
+                echo "<script>alert(\"有產品庫存不足\"); window.location.replace(\"nav.php#shop_order\");</script>";
+                exit();
+            }
+            else if (!$product_existence) {
+                echo "<script>alert(\"有產品已不存在\"); window.location.replace(\"nav.php#shop_order\");</script>";
+                exit();
+            }
+        }
     }
     else {
         $sql = $db->query("UPDATE `orders` SET `status` = 'finished', `finish_time` = current_timestamp() where `orders`.`OID` = '$OID'");
